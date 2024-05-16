bda267725da9d9cb8858c9b3dbf85b830b64ea66
Alex Harris <nharris@pdx.edu>
2024-05-16 14:49:23 -0700

    progress

--- hw3-sp24/src/components/navbar.tsx
+++ hw3-sp24/src/components/navbar.tsx
@@ -1,15 +1,25 @@
 export default function NavBar() {
+    const linkStyle = "text-dark rounded bg-light border-light py-1 px-2 me-2";
+    const linkStyleOnHover =
+        "text-dark rounded bg-light border-light py-1 px-2 me-2";
     return (
-        <nav className="bg-dark py-3">
+        <div className="bg-dark py-3">
             <div className="container">
-                <a href={`/`}>Home</a>
-
-                <a href={`/list`}>List</a>
-
-                <a href={`/population`}>Population</a>
-
-                <a href={`/customroute`}>Custom Route</a>
+                <nav>
+                    <a className={linkStyle} href={`/`}>
+                        Home
+                    </a>
+                    <a className={linkStyle} href={`/list`}>
+                        List
+                    </a>
+                    <a className={linkStyle} href={`/population`}>
+                        Population
+                    </a>
+                    <a className={linkStyle} href={`/customroute`}>
+                        Custom Route
+                    </a>
+                </nav>
             </div>
-        </nav>
+        </div>
     );
 }
